80b4d2fc72b17a84cdb0619eefba31f4f01ac6a4
Andy Holt <andy@altvia.com>
2021-11-08 13:43:22 -0700

    update client npm scripts

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Amazing Fibonacci Calculator</h1>
+          <h1 className="App-title">Amazing Fibonacci Calculator v2</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
